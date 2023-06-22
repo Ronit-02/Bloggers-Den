cba26def03d8e03922e49dec5b20cd10a4112daf
Ronit <ronitkhatri44@gmail.com>
2023-06-22 23:14:57 +0530

    fixing cors error

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,12 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000; 
 dotenv.config();
 app.use(express.json());
-app.use(
-    cors({
-        origin: '*'
-    })
-)
+app.use(cors())
+// app.use(
+//     cors({
+//         origin: '*'
+//     })
+// )
 
 // CONNECTING MONGODB
 mongoose.connect(process.env.MONGO_URL, {
